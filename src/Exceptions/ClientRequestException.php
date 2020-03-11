b7961be710b06fef0e57b46c724ca6a2f0abaf74
firdor <tim.sievers.onma@gmail.com>
2020-03-11 09:11:26 +0100

    Added method for a single profile query

--- src/Exceptions/ClientRequestException.php
+++ src/Exceptions/ClientRequestException.php
@@ -2,35 +2,7 @@
 
 namespace Kundentests\Exceptions;
 
-use GuzzleHttp\Psr7\Response;
-
 class ClientRequestException extends \Exception
 {
-  /**
-   * @var Response
-   */
-  protected $response;
-  
-  /**
-   * 
-   * @param Response $response
-   */
-  public function __construct(Response $response, \Throwable $previous = null)
-  {
-    $this->response = $response;
-    $this->previous = $previous;
-    
-    $this->message = $this->response->getBody();    
-    $this->code = $this->response->getStatusCode();
-  }
-    
-  /**
-   * 
-   * @return Response
-   */
-  public function getResponse() : Response
-  {
-    return $this->response;
-  }
 
 }
